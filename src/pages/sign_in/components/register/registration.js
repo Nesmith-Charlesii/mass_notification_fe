c9457b24a97d13e10568c4dcc4fad3d0df3c413d
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-29 15:11:04 -0400

    registration form created. working on styling

--- src/pages/sign_in/components/register/registration.js
+++ src/pages/sign_in/components/register/registration.js
@@ -6,7 +6,25 @@ const Registration = () => {
 
     return (
         <div className={classes.container}>
-            
+            <div className={classes.formContainer}>
+                <form className={classes.form}>
+                    <div className='form-group'>
+                        <input className='form-control-md' type="text" placeholder='First Name' style={{textAlign:"center", padding:"5px", fontFamily: "Cormorant Garamond", width: "100%"}}/>
+                    </div>
+                    <div className='form-group'>
+                        <input className='form-control-md' type="text" placeholder='Last Name' style={{textAlign:"center", padding:"5px", fontFamily: "Cormorant Garamond"}}/>
+                    </div>
+                    <div className='form-group'>
+                        <input className='form-control-md' type="text" placeholder='Title' style={{textAlign:"center", padding:"5px", fontFamily: "Cormorant Garamond"}}/>
+                    </div>
+                    <div className='form-group'>
+                        <input className='form-control-md' type="text" placeholder='Email' style={{textAlign:"center", padding:"5px", fontFamily: "Cormorant Garamond"}}/>
+                    </div>
+                    <div className='form-group'>
+                        <input className='form-control-md' type="text" placeholder='Phone number' style={{textAlign:"center", padding:"5px", fontFamily: "Cormorant Garamond"}}/>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
--- src/pages/sign_in/components/register/styles.js
+++ src/pages/sign_in/components/register/styles.js
@@ -3,10 +3,25 @@ import { createUseStyles } from "react-jss";
 const useStyles = createUseStyles({
     container: {
         height: "100vh",
-        backgroundColor: "rgb(45, 45, 45)",
+        backgroundColor: "rgb(30, 30, 30)",
         display: "flex",
         justifyContent: "center",
         alignItems: "center"
+    },
+    formContainer: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid rgb(200, 200, 200)",
+        borderRadius: "40px"
+    },
+    form: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        height: "50vh",
+        padding: "40px"
     }
 })
 
